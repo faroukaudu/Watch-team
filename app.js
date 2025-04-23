2e5aba4693f45edd8bd914a66d3f5715e467cf32
Farouk Audu <fagzy99@gmail.com>
2025-04-23 19:44:55 +0100

    run

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const myModule = require('./index.js');
 const app = myModule.main;
 
-app.listen(9000, function(req,res){
+app.listen(process.env.PORT || 9000, function(req,res){
     console.log("Watch TEAM is now running @ port 9000!");
   });
   
